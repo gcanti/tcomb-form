3c9a79b883e098985365a9027c316ab388633c24
gcanti <giulio.canti@gmail.com>
2015-11-18 14:00:49 +0100

    factor out renderers

--- src/templates/bootstrap/struct.js
+++ src/templates/bootstrap/struct.js
@@ -5,30 +5,46 @@ export default function struct(locals) {
   let children = [];
 
   if (locals.help) {
-    children.push(bootstrap.getAlert({
-      children: locals.help
-    }));
+    children.push(struct.renderHelp(locals.help));
   }
 
   if (locals.error && locals.hasError) {
-    children.push(bootstrap.getAlert({
-      type: 'danger',
-      children: locals.error
-    }));
+    children.push(struct.renderError(locals.error));
   }
 
   children = children.concat(locals.order.map(name => locals.inputs[name]));
 
-  let className = {};
+  const className = {};
   if (locals.className) {
     className[locals.className] = true;
   }
 
-  return bootstrap.getFieldset({
+  return struct.renderFieldset({
     className,
     disabled: locals.disabled,
     legend: locals.label,
     children
   });
-
 }
+
+struct.renderHelp = function (help) {
+  return bootstrap.getAlert({
+    children: help
+  });
+};
+
+struct.renderError = function (error) {
+  return bootstrap.getAlert({
+    type: 'danger',
+    children: error
+  });
+};
+
+struct.renderFieldset = function ({className, disabled, legend, children}) {
+  return bootstrap.getFieldset({
+    className,
+    disabled,
+    legend,
+    children
+  });
+};
